9eebb96bebdb5e400e3b8e2d7e55db212232d8ae
Jacqueline <jcasalia@yahoo.com.br>
2026-09-14 17:21:44 -0300

    criando o user com json

---
+++ README.md
@@ -0,0 +1,35 @@
+## Projeto
+E-commecer.
+
+O aplicativo ou sistema web deve possuir funcionalidades que permitam o cadastro, editar , ler os itens no cardápio.
+
+Esse projeto é o desenvolvimento da API Rest.
+
+## Imagem do Projeto
+![Video_2024-03-19_143950](./Video_2024-03-19_143950.gif)
+
+## ⚙️ Funcionalidades
+
+- [x] e-commecer;
+
+## 🛠 Tecnologias utilizadas
+
+- **[Java 17]**
+- **[Spring Boot 3]**
+- **[Maven]**
+- **[JSON]**
+- **[Lombok]**
+
+JSONLINT
+## Para Clonar o projeto
+https://github.com/JacquelineCasali/e-commecer
+
+# 📁 Acessar a aplicação
+
+##### Enquanto o servidor está ligado, acesse o navegador e entre no endereço abaixo
+http://localhost:8080/swagger-ui/index.html
+
+
+## 📝 Licença
+
+Projeto desenvolvido por CasaliTech.
---
+++ src/main/java/com/app/ecom/service/UserService.java
@@ -0,0 +1,44 @@
+package com.app.ecom.service;
+
+import com.app.ecom.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private List<User> userList = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public List<User> fetchAllUsers() {
+        return userList;
+    }
+
+
+    public void createUser(User user) {
+        user.setId(nextId++);
+        userList.add(user);
+
+    }
+
+    public boolean updateUser(Long id, User updateUser) {
+     return userList.stream()
+             .filter(user -> user.getId().equals(id))
+             .findFirst()
+             .map(existingUser ->{
+                 existingUser.setFristName(updateUser.getFristName());
+                 existingUser.setLastName(updateUser.getLastName());
+                 return true;
+             }).orElse(false);
+    }
+
+
+    public Optional<User> fetchUser(Long id) {
+       return userList.stream().filter(user -> user.getId().equals(id))
+               .findFirst();
+
+    }
+}
